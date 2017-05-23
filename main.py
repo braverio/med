23d2c5468199453a6054afdf7f6bc098b1791489
bnguyen654 <bnguyen9469@yahoo.com>
2017-05-23 02:04:17 -0400

    Inital

---
+++ main.py
@@ -0,0 +1,72 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, session, render_template, request, redirect, config, url_for
+import os
+import MySQLdb
+import hashlib
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = os.urandom(12)
+app.config.from_pyfile('config.cfg')
+
+@app.before_request
+def make_session_permanent():
+	app.permanent = True
+	app.permanent_session_lifetime = timedelta(minutes=5)
+
+@app.route('/')
+def index():
+	if 'uid' in session:
+		format_name = "%s, %s %s., %s" % (session['lname'],session['fname'],session['mi'],session['title'])
+		app.logger.error(format_name)
+		return render_template('index.html',format=format_name)
+	else:
+		return redirect(url_for('login'))
+	
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	if request.method == 'POST':
+		in_user = request.form['username']
+		in_pass = request.form['password']
+		db = MySQLdb.connect(app.config['DB_HOST'],app.config['DB_USER'],app.config['DB_PASS'],app.config['DB_NAME'])
+		c = db.cursor()
+		c.execute("SELECT salt FROM %s WHERE username='%s'" % (app.config['TABLE_LOGINS'],in_user))
+		if c.rowcount == 1:
+			salt = str(c.fetchone()[0])
+			app.logger.error(salt)
+			hashed = sha1_salt(in_pass,salt)
+			c = db.cursor()	
+			sql = "SELECT * FROM %s WHERE username='%s' AND password='%s'" % (app.config['TABLE_LOGINS'],in_user,str(hashed))
+			c.execute(sql)
+			app.logger.error(sql)
+			if c.rowcount == 1:
+				results = c.fetchall()
+				for row in results:
+					session['uid'] = row[0]
+					session['username'] = row[1]
+					session['fname'] = row[4]
+					session['lname'] = row[5]
+					session['mi'] = row[6]
+					session['title'] = row[7]
+
+				return redirect(url_for('index'))
+			else:
+				return render_template('login.html',user=in_user,error="Incorrect password. Please note that too many attempts will result in your account being locked.")
+		else:
+			return render_template('login.html',user=in_user,error="User not found. Please try again.")
+	elif request.method == 'GET':
+		if 'uid' in session:
+			return redirect(url_for('index'))
+		else:
+			return render_template('login.html')
+	
+
+if __name__ == "__main__":
+	app.run(debug=True,host='0.0.0.0')
+
+def sha1_salt(passwd,salt):
+	return hashlib.sha1(str(passwd).encode('utf-8')+str(salt).encode('utf-8')).hexdigest()
+
+
